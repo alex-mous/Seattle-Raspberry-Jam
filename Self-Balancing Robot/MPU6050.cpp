64d360950a6092c26c83f0ab21572a79905c73fc
Alex Mous <tree.pi.18@gmail.com>
2019-07-06 06:17:19 +0100

    Update MPU6050.cpp - now reliable angle readings

--- Self-Balancing Robot/MPU6050.cpp
+++ Self-Balancing Robot/MPU6050.cpp
@@ -1,6 +1,7 @@
-//C code to read the I2C data from the MPU6050 accelerometer/gyroscope
+//Copyright (c) 2019, Alex Mous
+//Licensed under the CC BY-NC SA 4.0
 
-//NOTE: work in progress
+//C code to read the I2C data from the MPU6050 accelerometer/gyroscope
 
 #define _POSIX_C_SOURCE 200809L
 
@@ -13,7 +14,7 @@
 #include <linux/i2c-dev.h>
 #include <cmath>
 
-#define TAU 0.02 //Complementary filter percentage
+#define TAU 0.05 //Complementary filter percentage
 #define RAD_T_DEG 57.29577951308 //Radians to degrees (180/PI)
 #define GYRO_SENS 131.0 //Gyroscope sensetivity factor
 #define ACCEL_SENS 16384.0 //Accelerometer sensetivity factor
@@ -21,24 +22,26 @@
 
 //Offsets - supply your own here (calculate offsets with readAccelOffsets function)
 //     Accelerometer
-#define A_OFF_X 16166.5
-#define A_OFF_Y 9630.44
-#define A_OFF_Z 7142.42
+#define A_OFF_X 19402
+#define A_OFF_Y -2692
+#define A_OFF_Z -8625
 //    Gyroscope
-#define G_OFF_X 0
-#define G_OFF_Y 0
-#define G_OFF_Z 0
+#define G_OFF_X -733
+#define G_OFF_Y 433
+#define G_OFF_Z -75
 
 int f_dev; //Device file
 int MPU6050_addr = 0x68;
 
-double gyro_angle; //Gyroscope angle
-double accel_angle; //Accelerometer angle
-double angle; //Final angle
-
 float dt = 0.009; //Loop time (recalculated with each loop)
 
 struct timespec start,end; //Create a time structure
+
+bool _first_run = 1; //Variable for whether to set gyro angle to acceleration angle in compFilter
+
+float accel_angle; //Accelerometer angle
+float gyro_angle; //Gyroscope angle
+float angle;
 
 
 int initMPU6050() { //General init function; opens dev file and does first configuration of MPU6050
@@ -118,7 +121,7 @@ void getAccel(float *x, float *y, float *z) { //Cleanup gyro data
 	*z = round((*z - A_OFF_Z) * 1000.0 / ACCEL_SENS) / 1000.0;
 }
 
-void getOffsets() { //Get accelerometer offsets
+void getOffsets() { //Get accelerometer offsets - place the module on a completely flat surface (check with a spirit level if possible) and make sure that is stays still while running this function
 	float gyro_off[3]; //Temporary storage
 	float accel_off[3];
 
@@ -129,7 +132,7 @@ void getOffsets() { //Get accelerometer offsets
 	accel_off_s[0] = 0, accel_off_s[1] = 0, accel_off_s[2] = 0;
 
 	for (int i = 0; i < 10000; i++) { //Use loop to average offsets
-		getGyro(&gyro_off[0], &gyro_off[1], &gyro_off[2]); //Raw gyroscope values
+		getGyroRaw(&gyro_off[0], &gyro_off[1], &gyro_off[2]); //Raw gyroscope values
 		gyro_off_s[0] = gyro_off_s[0] + gyro_off[0], gyro_off_s[1] = gyro_off_s[1] + gyro_off[1], gyro_off_s[2] = gyro_off_s[2] + gyro_off[2]; //Add to sum
 
 		getAccelRaw(&accel_off[0], &accel_off[1], &accel_off[2]); //Raw accelerometer values
@@ -139,59 +142,78 @@ void getOffsets() { //Get accelerometer offsets
 	gyro_off_s[0] = gyro_off_s[0] / 10000, gyro_off_s[1] = gyro_off_s[1] / 10000, gyro_off_s[2] = gyro_off_s[2] / 10000; //Divide by number of loops (average)
 	accel_off_s[0] = accel_off_s[0] / 10000, accel_off_s[1] = accel_off_s[1] / 10000, accel_off_s[2] = accel_off_s[2] / 10000;
 
-	std::cout << "Accelerometer Values: X " << accel_off[0] << ", Y " << accel_off[1] << ", Z" << accel_off[2] << "\nPut these in for A_OFF_X, A_OFF_Y and A_OFF_Z at the beginning of this code.";
-	std::cout << "Gyroscope Values: X " << gyro_off[0] << ", Y " << gyro_off[1] << ", Z" << gyro_off[2] << "\nPut these in for G_OFF_X, G_OFF_Y and G_OFF_Z at the beginning of this code.";
+	accel_off_s[2] = accel_off_s[2] - ACCEL_SENS; //Remove 1g from the value calculated to compensate for gravity)
+
+	std::cout << "Accelerometer Values: X " << accel_off_s[0] << ", Y " << accel_off_s[1] << ", Z" << accel_off_s[2] << "\nPut these in for A_OFF_X, A_OFF_Y and A_OFF_Z at the beginning of this code.\n\n\n";
+	std::cout << "Gyroscope Values: X " << gyro_off_s[0] << ", Y " << gyro_off_s[1] << ", Z" << gyro_off_s[2] << "\nPut these in for G_OFF_X, G_OFF_Y and G_OFF_Z at the beginning of this code.\n\n\n";
 }
 
-void compFilter() {
-	float ax, ay, az, gr, gp, gy;
+void compFilter(int axis, float *result) { //Set axis to 0,1 or 2 for R, P or Y measurents
+	float ax, ay, az, gr, gp, gy; //Raw values
 
-	getGyro(&gr, &gp, &gy);
+	getGyro(&gr, &gp, &gy); //Get the data from the sensors
 	getAccel(&ax, &ay, &az);
 
-	accel_angle = atan2(az, ax) * RAD_T_DEG; //Calculate the angle with the Z axis as y and X axis as x
+	accel_angle = atan2(az, ax) * RAD_T_DEG - 90.0; //Calculate the angle with z and x convert to degrees and subtract 90 degrees to rotate
+	gyro_angle = angle + gp*dt; //Use pitch axis (Y axis)
+/*	switch (axis) { //Switch which axis is used
+		case 0:
+			accel_angle = atan2(az, ay) * RAD_T_DEG - 90.0; //Calculate the angle with z and y convert to degrees and subtract 90 degrees to rotate
+			gyro_angle = gyro_angle + gr*dt; //Use roll axis (X axis)
+		case 1:
+			accel_angle = atan2(az, ax) * RAD_T_DEG - 90.0; //Calculate the angle with z and x convert to degrees and subtract 90 degrees to rotate
+			gyro_angle = gyro_angle + gp*dt; //Use pitch axis (Y axis)
+		case 2:
+			accel_angle = atan2(ay, ax) * RAD_T_DEG - 90.0; //Calculate the angle with y and x convert to degrees and subtract 90 degrees to rotate
+			gyro_angle = gyro_angle + gy*dt; //Use yaw axis (Z axis)
+	}*/
 
-//	gyro_angle = gyro_angle + gyro[1]*dt;
-	gyro_angle = angle + gyro[1]*dt;
-
-	asum = (abs(ax) + abs(ay) + abs(az); //Calculate the sum of the accelerations
-
-	if (asum > 0.1 && asum < 3) { //Check that the acceleration is not very high (therefore providing inacurate angles)
-		angle = (1 - TAU)*(gyro_angle) + (TAU)*(accel_angle); //Calculate the angle using a complementary filter
+	if (_first_run) { //Set the gyroscope angle reference point if this is the first function run
+		gyro_angle = accel_angle; //Start off with angle from accelerometer (absolute angle since gyroscope is relative)
+		_first_run = 0;
 	}
-	else {
-		angle = gyro_angle;
+
+	if (abs(gyro_angle - accel_angle) > 5) { //Correct for very large drift (or incorrect measurment of gyroscope by longer loop time)
+		gyro_angle = accel_angle;
 	}
 
-//	std::cout << "A: " << accel_angle << "\n";
-//	std::cout << "G: " << gyro_angle << "\n";
-//	std::cout << "R: " << angle << "\n";
-//	std::cout << "D: " << dt << "\n";
-//	std::cout << "AX  " << ax << "\n";
-//	std::cout << "AY  " << ay << "\n";
-//	std::cout << "AZ  " << az << "\n";
+	float asum = abs(ax) + abs(ay) + abs(az); //Calculate the sum of the accelerations
+	float gsum = abs(gr) + abs(gp) + abs(gy); //Calculate the sum of the gyro readings
 
-	std::cout << "GX  " << gr << "\n";
-	std::cout << "GY  " << gp << "\n";
-	std::cout << "GZ  " << gy << "\n";
+	//Create result from either complementary filter or directly from gyroscope or accelerometer depending on conditions
+	if (asum > 0.1 && asum < 3 && gsum > 0.3) { //Check that th movement is not very high (therefore providing inacurate angles)
+		*result = (1 - TAU)*(gyro_angle) + (TAU)*(accel_angle); //Calculate the angle using a complementary filter
+	}
+	else if (gsum > 0.3) { //Use the gyroscope angle if the acceleration is high
+		*result = gyro_angle;
+	}
+	else if (gsum <= 0.3) { //Use accelerometer angle if not much movement
+		*result = accel_angle;
+	}
+
+	std::cout << "A" << accel_angle << "\n";
+	std::cout << "G" << gyro_angle << "\n";
+	std::cout << "R" << *result << "\n";
+}
+
+int mainLoop() {
+	float res;
+	clock_gettime(CLOCK_REALTIME, &start); //Read current time into start variable
+
+	while (1) {
+		compFilter(1, &res); //Run filter function
+		clock_gettime(CLOCK_REALTIME, &end); //Save time to end clock
+		dt = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9; //Calculate new dt
+		clock_gettime(CLOCK_REALTIME, &start); //Save time to start clock
+	}
 }
 
 int main() {
 	initMPU6050();
 
-	//getOffsets();
+//	getOffsets();
 
-	clock_gettime(CLOCK_REALTIME, &start);
-
-	while (1) {
-		compFilter();
-
-
-		//Clock functions
-		clock_gettime(CLOCK_REALTIME, &end); //Save to end clock
-		dt = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9; //Calculate dt
-		clock_gettime(CLOCK_REALTIME, &start); //Save to start clock
-	}
+	mainLoop();
 
 	return 0;
 }
