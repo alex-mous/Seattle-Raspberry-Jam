c56fa7df38eb39a83b2bded2aee13987ad33bf94
Alex Mous <48971842+alexmous@users.noreply.github.com>
2019-09-03 16:28:34 -0700

    Update Hrdwr 7
    Add copyright notice in code and make some slight changes.

--- Hardware Project #7/clock.c
+++ Hardware Project #7/clock.c
@@ -1,3 +1,26 @@
+/*
+ * clock.c: userspace clock program using mincepi's pi2max2719 character device for the Raspberry Pi
+ * Copyright (c) 2019 mincepi <mincepi@gmail.com>
+ *
+ * Clock font is based on the Lat7-Terminus12x6 Linux console font.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * To compile run: gcc -o clock clock.c
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -11,7 +34,7 @@ time_t rawtime;
 struct tm *info;
 
 uint64_t font[80] = {	0x70, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x70,
-			0x20, 0x60, 0x20, 0x20, 0x20, 0x20, 0x20, 0x70,
+			0x40, 0xc0, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40,
 			0x70, 0x88, 0x88, 0x10, 0x20, 0x40, 0x80, 0xf8,
 			0x70, 0x88, 0x08, 0x30, 0x08, 0x08, 0x88, 0x70,
 			0x08, 0x18, 0x28, 0x48, 0x88, 0xf8, 0x08, 0x08,
@@ -24,21 +47,30 @@ uint64_t font[80] = {	0x70, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x70,
 //fill buf with current time in bitmap form
 void get() {
 
-    int i, hour;
+    int i;
+    int position = 48;
+    for(i = 0; i < 8; i++) buf[i] = 0;
 
     time(&rawtime);
     info = localtime(&rawtime);
-    for(i = 0; i < 8; i++) buf[i] = ((font[((info->tm_min % 10) * 8) + i]) << 26);
-    for(i = 0; i < 8; i++) buf[i] |= ((font[((info->tm_min / 10) * 8) + i]) << 32);
-    hour = info->tm_hour;
-    if (hour > 12) hour -= 12;
-    for(i = 0; i < 8; i++) buf[i] |= ((font[((hour % 10) * 8) + i]) << 42);
 
-    if ((hour / 10) != 0) for(i = 0; i < 8; i++) buf[i] |= ((font[((hour / 10) * 8) + i]) << 48);
+    //hours
+    if (info->tm_hour > 12) info->tm_hour -= 12;
+    if ((info->tm_hour / 10) != 0) for(i = 0; i < 8; i++) buf[i] |= ((font[((info->tm_hour / 10) * 8) + i]) << position);
+    if ((info->tm_hour / 10) == 1) {position -= 4;} else {position -= 7;}
+    for(i = 0; i < 8; i++) buf[i] |= ((font[((info->tm_hour % 10) * 8) + i]) << position);
+    if ((info->tm_hour % 10) == 1) {position -= 4;} else {position -= 7;}
 
     //colon
-    buf[2] |= ((uint64_t)1 << 42);
-    buf[5] |= ((uint64_t)1 << 42);
+    buf[2] |= ((uint64_t)0x80 << position);
+    buf[5] |= ((uint64_t)0x80 << position);
+    position -= 3;
+
+    //minutes
+    for(i = 0; i < 8; i++) buf[i] |= ((font[((info->tm_min / 10) * 8) + i]) << position);
+    if ((info->tm_min / 10) == 1) {position -= 4;} else {position -= 7;}
+    for(i = 0; i < 8; i++) buf[i] |= ((font[((info->tm_min % 10) * 8) + i]) << position);
+
     return;
 
 }
@@ -62,12 +94,12 @@ int main() {
 
         get();
 
-	//scroll and display
+	//scroll and display every ten seconds
 	for (i = 0; i < 40; i++) {
 
 	    send();
 	    for (j = 0; j < 8; j++) buf[j] <<= 1;
-	    usleep(70000);
+	    usleep(100000);
 
 	}
 
--- Hardware Project #7/pi2max7219.c
+++ Hardware Project #7/pi2max7219.c
@@ -1,3 +1,26 @@
+/*
+ * pi2max.c: userspace driver for the pi2max2719 character device for the Raspberry Pi
+ * Copyright (c) 2019 mincepi <mincepi@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * To compile install gpiod headers (apt-get install libgpiod-dev)
+ * then run: gcc -l gpiod -o pi2max7219 pi2max7219.c
+ *
+ */
+
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,7 +28,8 @@
 #include <gpiod.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-
+#include <sched.h>
+#include <sys/wait.h>
 
 struct gpiod_chip* chip;
 struct gpiod_line* clk;
@@ -14,7 +38,10 @@ struct gpiod_line* din;
 int bytes, value;
 char * line;
 char * byte;
-int len;
+FILE * file;
+size_t len;
+char *stack;
+char *top;
 
 //send 16 bit command to max7219
 void command(int command) {
@@ -41,17 +68,76 @@ void command(int command) {
 	usleep(1);
 
     }
-
+ 
     gpiod_line_set_value(cs, 1);
     usleep(1);
     return;
 
 }
 
+//get data from device file and send to display
+static int send(void *arg) {
+
+    int i;
+
+    for(;;) {
+
+	len = 0;
+	line = NULL;
+	bytes = 0;
+	getline(&line, &len, file);
+	byte = strtok(line, " ");
+
+	while (byte != NULL) {
+
+	    sscanf(byte, "%u", &value);
+    	    byte = strtok(NULL, " ");
+    	    bytes++;
+
+	    if (bytes < 9) {
+
+		value |= (bytes << 8);
+		gpiod_line_set_value(cs, 0);
+
+		for(i = 0; i < 16; i++) {
+
+		    if((value & (0x8000 >> i)) == 0) {
+
+			gpiod_line_set_value(din, 0);
+
+		    } else {
+
+			gpiod_line_set_value(din, 1);
+
+		    }
+
+		    gpiod_line_set_value(clk, 0);
+		    usleep(1);
+		    gpiod_line_set_value(clk, 1);
+		    usleep(1);
+
+		}
+ 
+		gpiod_line_set_value(cs, 1);
+		usleep(1);
+
+	    }
+
+	}
+
+	free(line);
+
+    }
+
+    fclose(file);
+    gpiod_chip_close(chip);
+    return(0);
+
+}
+
 int main(void) {
 
     int i;
-    FILE * file;
 
     //set up gpios
     chip = gpiod_chip_open_by_name("gpiochip0");
@@ -64,7 +150,7 @@ int main(void) {
 
     //blank display and initialize  max7219
     for(i = 0x100; i < 0x900; i += 0x100) command(i);
-    command(0xa0f);
+    command(0xa01); //intensity
     command(0xb07);
     command(0xc01);
     command(0xf00);
@@ -73,35 +159,12 @@ int main(void) {
     mkfifo("/dev/pi2max7219", 666);
     file = fopen("/dev/pi2max7219", "r+");
 
-    if (file <= 0) {
-        printf("\nError: insufficient user permissions!\n\n");
-	return 1;
-    }
-
-    //receive data and display it
-    for(;;) {
-
-	len = 0;
-	line = NULL;
-	bytes = 0;
-
-	getline(&line, &len, file);
-	byte = strtok(line, " ");
-
-	while (byte != NULL) {
-
-	    sscanf(byte, "%u", &value);
-    	    byte = strtok(NULL, " ");
-    	    bytes++;
-    	    if (bytes < 9) command((bytes << 8) | value);
-
-	}
-
-	free(line);
-
-    }
-
+    //launch background process
+    stack = malloc(1024 * 1024);
+    top = stack + (1024 * 1024);
+    clone(send, top, SIGCHLD, NULL);
     fclose(file);
     gpiod_chip_close(chip);
     return 0;
+
 }
